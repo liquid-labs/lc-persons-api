6ccd195a01f4b080738dfd36dab094ac2316d468
Zane Rockenbaugh <zane@liquid-labs.com>
2019-02-03 15:48:37 -0600

    ping with PathPrefix matching

--- go/persons/api.go
+++ go/persons/api.go
@@ -1,9 +1,20 @@
 package persons
 
 import (
+  "fmt"
+  "net/http"
+
   "github.com/gorilla/mux"
 )
 
-func InitAPI(r *mux.Router) {
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "/persons alive\n")
+}
 
+func InitAPI(r *mux.Router) {
+  // r.HandleFunc("/persons/", pingHandler).Methods("GET")
+
+  rUsers := r.PathPrefix("/persons/").Subrouter()
+
+  rUsers.HandleFunc("/", pingHandler).Methods("GET")
 }
