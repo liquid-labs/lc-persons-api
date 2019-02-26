6340df700c7b55517841f7330af741d65adeba50
Zane Rockenbaugh <zane@liquid-labs.com>
2019-02-26 14:52:54 -0600

    removed unused bin files

--- bin/users-api-service.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# bash strict settings
-set -o errexit # exit on errors
-set -o nounset # exit on use of uninitialized variable
-set -o pipefail
-
-npx --no-install go-dev-server.sh "$@"
